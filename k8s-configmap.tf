08ac1b91d74c428f816145b2ed67ddfa85f58b56
tlnob <thais.nobre7@gmail.com>
2026-03-07 14:37:49 -0300

    add aws secrets

--- k8s-configmap.tf
+++ k8s-configmap.tf
@@ -11,6 +11,8 @@ data:
   NODE_TLS_REJECT_UNAUTHORIZED: "0"
   REDIS_HOST: "redis-service.athena-tc5.svc.cluster.local"
   REDIS_PORT: "6379"
+  AWS_REGION: "us-east-1"
+  AWS_S3_BUCKET: "athena-videos-tc5-g192-v1"
 
 YAML
 }
